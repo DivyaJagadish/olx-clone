512b98e8a007b4adbc728833627be8f07855d550
DivyaJagadish <divyajagadish21@gmail.com>
2021-07-05 00:45:57 -0700

    Item and Item list Component using Material JUi

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Nav from "./Nav";
+import Itemlist from "./components/Itemlist";
 function App() {
 	return (
 		<>
 			<Nav />
+			<Itemlist />
 		</>
 	);
 }
