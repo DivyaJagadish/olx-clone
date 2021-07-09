f1864383ee3a23e60ee9e0cd9e8092f5690f1af3
DivyaJagadish <divyajagadish21@gmail.com>
2021-07-08 22:44:29 -0700

    Configures Api

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const Itemlist = [{"name":"HarryPotter", "Price": 100},{"name": "madhaviKutty","Price":"500"}]
-
+const Itemlist = [
+	{ name: "HarryPotter", Price: 100 },
+	{ name: "madhaviKutty", Price: "500" },
+];
+app.get("/api", (req, res) => res.json({ message: Itemlist }));
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
 });
