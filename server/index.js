ebb0d24b31f9e78115788fb70909add03354bc39
DivyaJagadish <divyajagadish21@gmail.com>
2021-07-14 23:06:05 -0700

    edits server

--- server/index.js
+++ server/index.js
@@ -2,10 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
-const Itemlist = [
-	{ name: "HarryPotter", Price: 100 },
-	{ name: "madhaviKutty", Price: "500" },
-];
 app.get("/api", (req, res) => res.json({ message: Itemlist }));
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
