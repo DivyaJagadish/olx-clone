e5362624ab08bdeaa9bd2f16353db5181d69eac1
DivyaJagadish <divyajagadish21@gmail.com>
2021-07-20 23:33:01 -0700

    Server Cleanup

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 8080;
 app.get("/api", (req, res) => res.json({ message: Itemlist }));
+// Server listening on port 8080
 app.listen(PORT, () => {
 	console.log(`Example app listening on port ${PORT}`);
 });
