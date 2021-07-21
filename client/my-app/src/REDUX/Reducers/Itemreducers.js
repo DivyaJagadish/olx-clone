bcd78d1d650defab2395a220c83cd15d2db0948d
DivyaJagadish <divyajagadish21@gmail.com>
2021-07-20 23:34:04 -0700

    Itemreducer  Cleanup

--- client/my-app/src/REDUX/Reducers/Itemreducers.js
+++ client/my-app/src/REDUX/Reducers/Itemreducers.js
@@ -4,10 +4,8 @@ export const itemReducer = (state, { type, payload }) => {
 	switch (type) {
 		case ActionTypes.Get_Items:
 			return state;
-			break;
 		case ActionTypes.Select_Item:
 			return state;
-			break;
 		default:
 			return state;
 	}
