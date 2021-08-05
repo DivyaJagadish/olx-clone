5e1f01033f6c1445f8519af1057a3d3e2291c54f
DivyaJagadish <divyajagadish21@gmail.com>
2021-08-04 23:16:19 -0700

    add value to state

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import "./App.css";
 import Nav from "./Nav";
 import Itemlist from "./components/Itemlist";
+import useApplicationData from "./helpers/useApplicationdata";
 // import { Provider } from "react-redux";
 // import store from "./REDUX/store";
 
 function App() {
+	const { state } = useApplicationData();
 	return (
 		<>
 			<Nav />
--- client/my-app/src/helpers/useApplicationdata.js
+++ client/my-app/src/helpers/useApplicationdata.js
@@ -1,18 +1,18 @@
-import React, { useEffect } from "react";
-import useState from " react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function useApplicationData() {
 	const [state, setState] = useState({
 		products: [],
 	});
 	useEffect(() => {
-		const Products = axios.get("/api/digitalpet");
-
+		const Products = axios.get("https://fakestoreapi.com/products");
+		console.log("ddddd");
 		Products.then((result) => {
-			console.log(result);
+			console.log(result.data);
 			setState((prev) => ({
 				...prev,
-				products: result,
+				products: result.data,
 			}));
 		});
 	}, []);
