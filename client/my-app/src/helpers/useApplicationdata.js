69ac7bb4fdfa07cc25620680238e72badaf341ab
DivyaJagadish <divyajagadish21@gmail.com>
2021-08-05 16:58:59 -0700

    Create product Context

--- client/my-app/src/helpers/useApplicationdata.js
+++ client/my-app/src/helpers/useApplicationdata.js
@@ -7,9 +7,7 @@ export default function useApplicationData() {
 	});
 	useEffect(() => {
 		const Products = axios.get("https://fakestoreapi.com/products");
-		console.log("ddddd");
 		Products.then((result) => {
-			console.log(result.data);
 			setState((prev) => ({
 				...prev,
 				products: result.data,
