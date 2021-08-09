58c36f3fed724beea22bb1cb38e7bfd14d135ebd
DivyaJagadish <divyajagadish21@gmail.com>
2021-08-08 18:39:38 -0700

    Adds React router

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import useApplicationData from "./helpers/useApplicationdata";
 import "./components/styles/itemlist.css";
 import ItemDescriptionPage from "./pages/Itemdescriptionpage";
 import ProductContext from "./Contexts/ProductContext";
+import Homepage from "./pages/HomePage";
 // import { Provider } from "react-redux";
 // import store from "./REDUX/store";
 
@@ -13,7 +15,16 @@ function App() {
 	return (
 		<>
 			<ProductContext.Provider value={state.products}>
-				<ItemDescriptionPage />
+				<Router>
+					<Switch>
+						<Route exact path="/">
+							<Homepage />
+						</Route>
+						<Route exact path="/details">
+							<ItemDescriptionPage />
+						</Route>
+					</Switch>
+				</Router>
 			</ProductContext.Provider>
 		</>
 	);
---
+++ client/my-app/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Nav from "../components/Nav";
+import Itemlist from "../components/Itemlist";
+
+function Homepage() {
+	return (
+		<>
+			<Nav />
+			<Itemlist />
+		</>
+	);
+}
+
+export default Homepage;
