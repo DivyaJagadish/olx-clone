7dd17d3e9a27a4eeb9d52e2db75368c049630a72
DivyaJagadish <divyajagadish21@gmail.com>
2021-08-08 17:38:10 -0700

    Item description page

--- client/my-app/src/components/Itemdescription.js
+++ client/my-app/src/components/Itemdescription.js
@@ -10,11 +10,25 @@ import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { lightBlue } from "@material-ui/core/colors";
+import { FormHelperText } from "@material-ui/core";
 
 const useStyles = makeStyles({
 	root: {
+		height: 600,
+		width: 600,
+	},
+	media: {
+		display: "flex",
+		justifyItems: "center",
 		height: 400,
-		width: 350,
+		width: "80%",
+		objectFit: "scale-down",
+	},
+	Typography: {
+		fontSize: "15px",
+	},
+	Button: {
+		padding: "2rem",
 	},
 });
 
@@ -22,17 +36,46 @@ export default function Itemdescription(props) {
 	const Items = useContext(ProductContext);
 	const classes = useStyles();
 	const data = Items[props.index];
+	function description() {
+		if (data) {
+			return (
+				<>
+					<Typography gutterBottom variant="h3" component="h2">
+						{data.title}
+					</Typography>
+					<Card className={classes.root}>
+						<CardActionArea className={classes.media}>
+							<Typography gutterBottom variant="h5" component="h2">
+								{data.title}
+							</Typography>
+							<CardMedia
+								className={classes.media}
+								component="img"
+								image={data.image}
+								title={data.title}
+							/>
+						</CardActionArea>
+						<CardContent>
+							<Typography gutterBottom variant="h5" component="h2">
+								${data.price}
+							</Typography>
+							<Typography>{data.description}</Typography>
+						</CardContent>
+
+						<CardActions>
+							<Button variant="contained" size="small" color="primary">
+								Add to Cart
+							</Button>
+							<Button variant="contained" size="small" color="primary">
+								Back
+							</Button>
+						</CardActions>
+					</Card>
+				</>
+			);
+		} else return <>{"Loading"}</>;
+	}
+
 	console.log(data);
-	return (
-		<>
-			{" "}
-			{{
-				if(data) {
-					data.title;
-					data.img;
-				},
-			}}
-			Hello
-		</>
-	);
+	return <>{description()}</>;
 }
