0e3034a51888251a1bfef3f4ff3da293126d7f00
DivyaJagadish <divyajagadish21@gmail.com>
2021-09-21 23:15:58 -0700

    Nav bar

--- client/my-app/src/components/Nav.js
+++ client/my-app/src/components/Nav.js
@@ -4,6 +4,8 @@ import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import Icon from "@mui/material/Icon";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
@@ -26,20 +28,15 @@ export default function Nav() {
 		<div className={classes.root}>
 			<AppBar position="static">
 				<Toolbar>
-					<IconButton
-						edge="start"
-						className={classes.menuButton}
-						color="inherit"
-						aria-label="menu"
-					>
-						<MenuIcon />
-					</IconButton>
+					<Link to={"/"}>
+						<HomeOutlinedIcon />
+					</Link>
 					<Typography variant="h6" className={classes.title}>
 						Estore
 					</Typography>
-					<Link to={"/mycart"}>
-						<Button color="inherit">MyCart</Button>
-					</Link>
+					<Button color="inherit">
+						<Link to={"/mycart"}>MyCart</Link>
+					</Button>
 					<Button color="inherit">Login</Button>
 				</Toolbar>
 			</AppBar>
